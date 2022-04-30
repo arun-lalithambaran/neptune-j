5d04a69c622667f40c24b0a47e27f4670dba28ed
arun.lalithambaran <arun.lalitambaran@feathersoft.com>
2022-04-30 16:13:15 +0530

    new initialization files added

---
+++ src/test/java/com/neptune/afo/AfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptune.afo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
